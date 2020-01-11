fd4dd64eb6e1262b953bc6fd39dc9dd16856d1f7
David Ornelles <ornelles@wakehealth.edu>
2020-01-11 14:16:28 -0500

    revised peakFind dnaplot code

--- R/dnaplot.R
+++ R/dnaplot.R
@@ -4,24 +4,35 @@
 #' DNA profiles. 
 #' 
 #' @md
-#' @param ... Arguments to be handed to [flowViz::densityplot()]
-#' @param data `flowFrame` or `flowSet` with data
-#' @param adjust Numeric factor to adjust the bandwith in the kernel
-#'   density function [stats::density()], decreased from 1 to 0.2
-#' @param n The number of equally spaced points at which the density is to
-#'   be estimated, increased from 50 to 512
+#' @param ... Arguments to be handed to [flowViz::densityplot()] *or*
+#'   a `flowFrame` or `flowSet`
+#' @param darg List of arguments passed to the kernel density function
+#'   [stats::density()] to increase the resolution of the plot. In
+#'   addition to `bw`, these can include `adjust`, `n`. The option
+#'   `na.rm = TRUE` will be included in all cases
+#' @param chan An option character scalar identifying the data to be
+#'   plotted if `...` is a `flowFrame` or `flowSet`
 #' @param plot If `TRUE`, lattice object will be plotted as well as 
 #'   being returned as an `invisible` object
 #'
 #' @details
 #' This is a convenience function with parameters more suited for
-#' typical DNA profiles. Specifically, the optional arguments to
-#' [stats::density()], which are provided in `darg`, are changed from
-#' `n = 50` and `adjust = 1` to `n = 512` and `adjust = 0.2`.
+#' typical DNA profiles by using the bandwidth algorithm of Sheather
+#' & Jones in `bw.SJ` with the adjustment in `adjust` and using more
+#' points (512 rather than 50) to determine the density. These additional
+#' parameters are passed to [stats::density()], which in `darg`.
 #'
 #' The call effectively executes the following:
 #' ```
-#'   densityplot(..., dargs = list(adjust = adjust, n = n))
+#'   densityplot(..., dargs = dargs)
+#' ```
+#' ... **except** for the *lazy* case of calling it with first argument a
+#' `flowSet` or `flowFrame`. In this case the function guesses
+#' the most likely `chan` parameter (in order) from `FL2.A, FL3.A, FL2.H`
+#' or `FL3.H`. Alternatively, the channel name can be passed in `chan` and
+#' the call now becomes:
+#' ```
+#'   densityplot(~ chan, ..., dargs = dargs)
 #' ```
 #' 
 #' @return
@@ -29,19 +40,49 @@
 #' return the lattice plot.
 #' 
 #' @examples 
-#'  fs <- readSet(system.file("extdata", "RPE_synch/", package = "flowExtra"))
-#'  dnaplot(~ FL2.A, fs) # unadjusted values
+#' # load and filter example data with 16 flowFrames
+#'   fs <- readSet(system.file("extdata", "RPE_synch/", package = "flowExtra"))
+#'   fs <- Subset(fs, boundaryFilter("FL2.A"))
+#' # default density parameters from flowViz 
+#'   dnaplot(~ FL2.A, fs, xlim = c(50, 500), darg = NULL, main = "Original")
+#' # revised density parameters 
+#'   dnaplot(~ FL2.A, fs, xlim = c(50, 500), main = "Revised")
+#' # lazy use...
+#'   dnaplot(fs)
 #'
 #' @import flowViz
 #'
 #' @export
 #'
-dnaplot <- function(..., adjust = 0.2, n = 512, plot = TRUE)
+dnaplot <- function(..., darg = list(bw = "SJ", n = 512, adjust = 1),
+	chan = NULL, plot = TRUE)
 {
+# extract first argument
 	dots <- list(...)
-	darg <- list(adjust = adjust, n = n)
+
+# special case of lazily treating this like an S4 function...
+	if (is(dots[[1]], "flowFrame") | is(dots[[1]], "flowSet")) {
+		x <- dots[[1]]
+		if (is.null(chan)) {
+			choices <- c("FL2.A", "FL3.A", "FL2.H", "FL3.H")
+			sel <- pmatch(colnames(x), choices)
+			if (all(is.na(sel)))
+				stop("need one of: ", paste(choices, collapse = ", "), " in data")
+			else	
+				chan <- choices[min(sel, na.rm = TRUE)]
+		}
+		else if (!chan %in% colnames(x))
+			stop ('"', deparse(substitute(chan)), '" not found')
+		form <- as.formula(paste("~", chan))
+		dots <- c(form, dots)
+	}
+# ensure that 'na.rm = TRUE' is in 'darg'
+	if (!"na.rm" %in% names(darg))
+		darg <- c(darg, na.rm = TRUE)
+# make call
 	argList <- c(dots, darg = list(darg))
 	obj <- do.call(flowViz::densityplot, args = argList)
+# plot object unless told not to...
 	if (plot == TRUE)
 		plot(obj)
 	invisible(obj)
--- R/peakFind.R
+++ R/peakFind.R
@@ -1,47 +1,45 @@
-#' Find G1 and G2 peaks
+#' Find G1 and G2/M peaks
 #' 
-#' Find G1 and G2 (and other) peaks in FL2.A values
+#' Find G1 and G2/M (and other) peaks in FL2.A values
 #' 
 #' @md
 #' @details
 #' This function identifies the probable location of the G1 and G2/M peaks
-#' in the `"FL2.A"` channel (or the channel
-#' specified by the argument `chan`) in a `flowFrame` or `flowSet`. Areas
-#' of high density are identified by the function defined in `curveFilter`. If 
-#' this is `NULL`, the [flowStats::curv1Filter()] function will be used 
-#' with optional arguments (`bwFac` or `gridsize`) passed in `...`.
+#' in the channel identified by `chan` in either a `flowFrame` or `flowSet`.
+#' Areas of local high density are first identified by
+#' [flowStats::curv1Filter()] with arguments in `bwFac` and `gridsize`.
+#' The values are the default values for by `curv1Filter()`. 
+#' The peak location within each local area of high density is
+#' identified from a kernel density estimate [stats::density()] using
+#' arguments passed in the list `darg`. Peak positions are returned as
+#' an array of two or more columns.
 #' 
-#' If `range.search` is `NULL`, peaks are found accepted in the range of data
-#' spanning  5th to 95th percentile or to the percentile values specified in
-#' `probs`. For well-behaved data, `range.search` can be set to `c(50, 500)` to
-#' exclude apoptotic and polyploid populations.
-#' If no more than two peaks
-#' are found, they will be labeled `"G1"` and `"G2"`. A single peak will be
-#' labeled as `"G1"` with a  value of `NA` for the second peak, which will
-#' be labeled as `"<G2>"`. If more than two peaks are found, the peaks will be
-#' labeled as `"peak1"`, `"peak2"`, `"peak3"`, etc.
+#' If `range.search` is `NULL`, peaks are accepted in the range of data
+#' spanning 5th to 95th percentile or as specified by the percentile values
+#' in `probs`. For well-behaved data, `range.search` can be set
+#' to `c(50, 500)` to exclude apoptotic and polyploid populations.
+#'
+#' If exactly two peaks are found, they will be labeled `"G1"` and `"G2"`.
+#' If one peak is found, it will be treated as the "`G1`" peak and
+#' the `"G2"` peak will be assigned an `NA` value. If more than two peaks
+#' are found, the peaks will be labeled as `"peak1"`, `"peak2"`, `"peak3"`,
+#' etc. A warning message will report finding other than 2 peaks.
 #'
 #' @param x A `flowFrame` or `flowSet` with the parameter named in `chan`
-#' @param curveFilter An optional function that returns a `multipleFilterResult`
-#'   defining the regions of 1-D curvature in `chan`. If `NULL`, the
-#'   [flowStats::curv1Filter()] will be used
-#' @param chan The name of the data to be evaluated in `x`, default of `"FL2.A"`
-#' @param searchFun An optional function to determine the position of each
-#'   peak defined by application of `curvFilter`. If `NULL`, the
-#'   [stats::density()] function will be used with the bandwith adjusted
-#'   by the option `adj = 2`
-#' @param range.search An optional numeric vector of length 2 defining the
-#'   lower and upper limits to accept peaks. The default value of `NULL` 
+#' @param chan The name of the data to be evaluated in `x`
+#' @param darg List of arguments passed to the kernel density estimating
+#'   function ([stats::density()]) used to find peaks
+#' @param range.search An optional numeric vector of length 2 setting the
+#'   range of values in `chan` to search for peaks. The default value of `NULL` 
 #'   limits the search to the range defined by the quantiles specified
 #'   in `probs`
 #' @param probs Numeric vector of length 2 defining the lower and upper
-#'   quantiles for valid peak values, the default value of 5 to 95\%
-#'   is specified by the argument `probs = c(0.05, 0.95)`
-#' @param ... Additional arguments to be passed to
-#'   [flowStats::curv1Filter()]
+#'   quantiles to set the value for `range.search` if not provided
+#' @param bwFac,gridsize Numeric values of length 1 and 2 used by
+#'   [flowStats::curv1Filter()] to identify regions of 1-D curvature in
 #' 
 #' @return
-#' An 1-D or 2-D array with rownames obtained from [flowCore::identifier()]
+#' An 2-D array with row names obtained from [flowCore::identifier()]
 #' and column names determined as described above.
 #' 
 #' @examples 
@@ -53,23 +51,29 @@
 #' 
 #' @export
 #' 
-peakFind <- function(x, curveFilter = NULL, chan = "FL2.A",
-		searchFun = NULL, range.search = NULL, probs = c(0.05, 0.95), ...)
+peakFind <- function(x, chan = "FL2.A", darg = list(bw = "nrd0", n = 512),
+		range.search = NULL, probs = c(0.05, 0.95), bwFac = 1.2,
+		gridsize = rep(401, 2))
 {
-	# assign values to arguments
-	if (is.null(curveFilter))
-		curveFilter <- curv1Filter(chan, ...)
-	if (is.null(searchFun)) # kernel density with 2x default bandwidth
-		searchFun <- function(x, adj = 2) {
-			kd <- stats::density(x, adj = adj, na.rm = TRUE)
-			kd$x[which.max(kd$y)]	# return first 'x' at maximum
-		}
+	# argument check
+		if (!any(is(x, "flowSet"), is(x, "flowFrame")))
+			stop("'", deparse(substitute(x)), "' must be a flowFrame or a flowSet")
+		if (!chan %in% colnames(x))
+			stop('"', chan, '" is not in ', deparse(substitute(x)))
+
 # working function to perform peak finding on a flowFrame
-	.peakFind <- function(x, curveFilter, chan, searchFun, range.search)
+	.peakFind <- function(x, chan, range.search, darg)
 	{
-	# apply filter to define high density regions in one dimension (curv1Filter)
-		res <- filter(x, curveFilter)
-		peaks <- tapply(exprs(x)[, chan], res@subSet, searchFun)[-1]
+	# identify high-density regions with curv1Filter
+		c1f <- curv1Filter(list(chan), bwFac = bwFac, gridsize = gridsize)
+		res <- flowCore::filter(x, c1f)
+	# helper function to pick peak position
+		.localMax <- function(x) {
+			kd <- do.call(stats::density, c(list(x), darg))
+			kd$x[which.max(kd$y)] }
+	# find the peak within each high density region
+		peaks <- tapply(exprs(x)[, chan], res@subSet, .localMax)
+		peaks <- peaks[-1] # drop "rest"
 	# assign value to 'range.search' if needed
 		if (is.null(range.search))
 			range.search <- quantile(exprs(x)[, chan], probs = probs, na.rm = TRUE)
@@ -77,36 +81,33 @@ peakFind <- function(x, curveFilter = NULL, chan = "FL2.A",
 		peaks <- peaks[!is.na(peaks)]
 		sel <- peaks > range.search[1] & peaks < range.search[2]
 		peaks <- peaks[peaks > range.search[1] & peaks < range.search[2]]
-	# assign best guess names to peaks
-		if (length(peaks) == 2) # assume G1 and G2 peak found
-			names(peaks) <- c("G1", "G2")
-		else if (length(peaks) == 1) { # assume only G1 peak found
+	# add NA value for only one peak
+		if (length(peaks) == 1) { # add NA for G2 peak
 			peaks[2] <- NA
-			names(peaks) <- c("G1", "<G2>")
 			warning(identifier(x), ": one peak found", call. = FALSE)
 		}
-		else {
+		else if (length(peaks) > 2) # report
 			warning(identifier(x), ": ", length(peaks), " peaks found", call. = FALSE)
-			names(peaks) <- paste("peak", 1:length(peaks), sep="")
-		}
 		return(peaks)	# return x-position of peaks
 	}
 
 # dispatch working function according to arguments
 	if (class(x) == "flowFrame") {
-		ans <- t(.peakFind(x, curveFilter, chan, searchFun, range.search))
+		ans <- t(.peakFind(x, chan, range.search, darg))
 		rownames(ans) <- identifier(x)
 	}
 	else if (class(x) == "flowSet") {
-		ans <- fsApply(x, .peakFind, curveFilter, chan, searchFun, range.search,
-				simplify = FALSE)
-		ans.range <- range(sapply(ans, length), na.rm = TRUE)
-		cnames <- names(ans[[which(sapply(ans, length) == ans.range[2])[1]]])
-		ans <- lapply(ans, function(x) c(x, rep(NA, ans.range[2] - length(x))))
-		ans <- t(as.data.frame(ans))
-		colnames(ans) <- cnames
+		ans <- fsApply(x, .peakFind, chan, range.search, darg, simplify = FALSE)
+		n <- max(lengths(ans), na.rm = TRUE)
+		ans <- t(sapply(ans, function(x) c(x, rep(NA, n))[1:n]))
 	}
 	else
-		stop("unable to operate on argument of class ", class(x))
-	return(ans) # either 1-D or 2-D array
+		stop("SHOULDN'T BE HERE with argument of class ", class(x))
+	if (ncol(ans) == 2)
+		colnames(ans) <- c("G1", "G2")
+	else if (ncol(ans) > 2)
+		colnames(ans) <- paste0("peak", seq_len(ncol(ans)))
+	else
+		warning("SHOULD'NT BE HERE with 1-D array")
+	return(ans) # array of 2 or more dimensions
 }
