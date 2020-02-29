0dd1eea19c9678b154466aebf271961bd5e33a4c
David Ornelles <ornelles@wakehealth.edu>
2020-02-29 15:45:17 -0500

    simplify argument options

--- R/dnaplot.R
+++ R/dnaplot.R
@@ -6,49 +6,45 @@
 #' @md
 #' @param ... Arguments to be handed to [flowViz::densityplot()] *or*
 #'   a `flowFrame` or `flowSet`
-#' @param darg List of arguments passed to the kernel density function
-#'   [stats::density()] to increase the resolution of the plot. In
-#'   addition to `bw`, these can include `adjust`, `n`. The option
-#'   `na.rm = TRUE` will be included in all cases
 #' @param chan An option character scalar identifying the data to be
 #'   plotted if `...` is a `flowFrame` or `flowSet`
-#' @param plot If `TRUE`, lattice object will be plotted as well as 
-#'   being returned as an `invisible` object
+#' @param bw,n,adjust Parameters passed to the kernel density function
+#'   [stats::density()] to increase the resolution of the plot with
+#'   default values of `bw = "SJ"`, `n = 512` and `adjust = 1`. The option
+#'   `na.rm = TRUE` will be included in all cases
 #'
 #' @details
 #' This is a convenience function with parameters more suited for
 #' typical DNA profiles by using the bandwidth algorithm of Sheather
 #' & Jones in `bw.SJ` with the adjustment in `adjust` and using more
-#' points (512 rather than 50) to determine the density. These additional
-#' parameters are passed to [stats::density()], which in `darg`.
+#' points (512 rather than 50) to determine the density. 
 #'
 #' The call effectively executes the following:
 #' ```
-#'   densityplot(..., dargs = dargs)
+#' densityplot(..., dargs = list(bw = bw, n = n, adjust = adjust, na.rm = TRUE))
 #' ```
-#' ... **except** for the *lazy* case of calling it with first argument a
+#' **Except** for the *lazy* case of calling it with first argument a
 #' `flowSet` or `flowFrame`. In this case the function guesses
 #' the most likely `chan` parameter (in order) from `FL2.A, FL3.A, FL2.H`
 #' or `FL3.H`. Alternatively, the channel name can be passed in `chan` and
 #' the call now becomes:
 #' ```
-#'   densityplot(~ chan, ..., dargs = dargs)
+#'   densityplot(~ chan, ..., dargs = list(bw = bw, n = n, adjust = adjust, na.rm = TRUE))
 #' ```
 #' 
 #' @return
-#' Optionally plot the data as a side-effect and invisibly
-#' return the lattice plot.
+#' Plot the data and invisibly return the lattice object.
 #' 
 #' @examples 
 #' # load and filter example data with 16 flowFrames
 #'   fs <- readSet(system.file("extdata", "synch/", package = "flowExtra"))
 #'   fs <- Subset(fs, boundaryFilter("FL2.A"))
 #' # default density parameters from flowViz 
-#'   dnaplot(~ FL2.A, fs, xlim = c(50, 500), darg = NULL,
+#'   densityplot(~ FL2.A, fs, xlim = c(50, 500),
 #'     main = "Default parameters for densityplot")
 #' # revised density parameters 
 #'   dnaplot(~ FL2.A, fs, xlim = c(50, 500),
-#'     main = "Adjusted parameters for 'dnaplot'")
+#'     main = "Refined parameters for 'dnaplot'")
 #' # lazy use if "FL2.A" is present
 #'   dnaplot(fs)
 #'
@@ -56,10 +52,9 @@
 #'
 #' @export
 #'
-dnaplot <- function(..., darg = list(bw = "SJ", n = 512, adjust = 1),
-	chan = NULL, plot = TRUE)
+dnaplot <- function(..., chan = NULL, bw = "SJ", n = 512, adjust = 1)
 {
-# extract first argument
+# extract arguments that are not passed to density()
 	dots <- list(...)
 
 # special case of lazily treating this like an S4 function...
@@ -78,14 +73,15 @@ dnaplot <- function(..., darg = list(bw = "SJ", n = 512, adjust = 1),
 		form <- as.formula(paste("~", chan))
 		dots <- c(form, dots)
 	}
-# ensure that 'na.rm = TRUE' is in 'darg'
-	if (!"na.rm" %in% names(darg))
-		darg <- c(darg, na.rm = TRUE)
+
+# extract three arguments for density()
+	darg <- list(bw = bw, n = n, adjust = adjust, na.rm = TRUE)
+
 # make call
 	argList <- c(dots, darg = list(darg))
 	obj <- do.call(flowViz::densityplot, args = argList)
-# plot object unless told not to...
-	if (plot == TRUE)
-		plot(obj)
+
+# plot and return invisible object
+	plot(obj)
 	invisible(obj)
 }
